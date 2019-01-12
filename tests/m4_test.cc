205f1550376fb14055bdbd3508d39536b27fe30e
John Millikin <john@john-millikin.com>
2019-01-12 13:08:58 -0800

    Use Bazel runfiles helper for locating test data.

--- tests/m4_test.cc
+++ tests/m4_test.cc
@@ -2,38 +2,51 @@
 #include <iostream>
 
 #include "gtest/gtest.h"
+#include "tools/cpp/runfiles/runfiles.h"
 
-namespace {
+using bazel::tools::cpp::runfiles::Runfiles;
+using std::string;
 
-std::string ReadFile(const std::string& path) {
-    std::ifstream fp(path);
-    std::stringstream buf;
-    buf << fp.rdbuf();
-    return buf.str();
-}
+class RulesM4 : public ::testing::Test {
+  protected:
+    void SetUp() override {
+        string error;
+        runfiles_.reset(Runfiles::CreateForTest(&error));
+        ASSERT_EQ(error, "");
+    }
 
-}  // namespace
+    string ReadFile(const string& path) {
+        string resolved_path = runfiles_->Rlocation(path);
+        std::ifstream fp(resolved_path);
+        EXPECT_TRUE(fp.is_open());
+        std::stringstream buf;
+        buf << fp.rdbuf();
+        return buf.str();
+    }
 
-TEST(RulesM4, ExpansionTest) {
+    std::unique_ptr<Runfiles> runfiles_;
+};
+
+TEST_F(RulesM4, ExpansionTest) {
     const char EXPECT[] = R"(#line 2 "tests/expansion_test.in"
 Hello, world!
 )";
-    const auto got = ReadFile("./tests/expansion_test.txt");
+    const auto got = ReadFile("io_bazel_rules_m4/tests/expansion_test.txt");
     ASSERT_EQ(got, EXPECT);
 }
 
-TEST(RulesM4, GenruleTest) {
+TEST_F(RulesM4, GenruleTest) {
     const char EXPECT[] = R"(#line 2 "genrule_test.in"
 Hello, world!
 )";
-    const auto got = ReadFile("./tests/genrule_test.txt");
+    const auto got = ReadFile("io_bazel_rules_m4/tests/genrule_test.txt");
     ASSERT_EQ(got, EXPECT);
 }
 
-TEST(RulesM4, RecoveryTest) {
+TEST_F(RulesM4, RecoveryTest) {
     const char EXPECT[] = R"(from template_1: Hello, world!
 from template_2: Hello, world (again)!
 )";
-    const auto got = ReadFile("./tests/template_test");
+    const auto got = ReadFile("io_bazel_rules_m4/tests/template_test");
     ASSERT_EQ(got, EXPECT);
 }
