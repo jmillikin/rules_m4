b22f8498d2557bbe9afdaad78d6a8f760fce1853
John Millikin <john@john-millikin.com>
2022-04-27 15:49:03 +0900

    Use environment variables to locate test data.
    GitOrigin-RevId: c13c762f5b1f9830bad8340c76a28c9aa078dc28

--- tests/m4_test.cc
+++ tests/m4_test.cc
@@ -24,14 +24,39 @@ class RulesM4 : public ::testing::Test {
         return buf.str();
     }
 
+    string TestsDir() {
+        const char *test_binary_ptr = getenv("TEST_BINARY");
+        const char *test_workspace_ptr = getenv("TEST_WORKSPACE");
+
+        if (test_binary_ptr == nullptr) {
+            EXPECT_NE(test_binary_ptr, nullptr);
+            return "";
+        }
+        if (test_workspace_ptr == nullptr) {
+            EXPECT_NE(test_workspace_ptr, nullptr);
+            return "";
+        }
+
+        string test_binary(test_binary_ptr);
+        string test_workspace(test_workspace_ptr);
+
+        size_t slash = test_binary.find_last_of('/');
+        if (slash == string::npos) {
+            EXPECT_NE(slash, string::npos);
+            return "";
+        }
+
+        return test_workspace + "/" + test_binary.substr(0, slash);
+    }
+
     std::unique_ptr<Runfiles> runfiles_;
 };
 
 TEST_F(RulesM4, ExpansionTest) {
-    const char EXPECT[] = R"(#line 2 "tests/expansion_test.in"
+    const char EXPECT[] = "#line 2 \"" EXPANSION_TEST_IN R"("
 Hello, world!
 )";
-    const auto got = ReadFile("rules_m4/tests/expansion_test.txt");
+    const auto got = ReadFile(TestsDir() + "/expansion_test.txt");
     ASSERT_EQ(got, EXPECT);
 }
 
@@ -39,7 +64,7 @@ TEST_F(RulesM4, GenruleTest) {
     const char EXPECT[] = R"(#line 2 "genrule_test.in"
 Hello, world!
 )";
-    const auto got = ReadFile("rules_m4/tests/genrule_test.txt");
+    const auto got = ReadFile(TestsDir() + "/genrule_test.txt");
     ASSERT_EQ(got, EXPECT);
 }
 
@@ -47,6 +72,6 @@ TEST_F(RulesM4, RecoveryTest) {
     const char EXPECT[] = R"(from template_1: Hello, world!
 from template_2: Hello, world (again)!
 )";
-    const auto got = ReadFile("rules_m4/tests/template_test");
+    const auto got = ReadFile(TestsDir() + "/template_test");
     ASSERT_EQ(got, EXPECT);
 }
